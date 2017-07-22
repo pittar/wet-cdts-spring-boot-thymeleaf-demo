53015aad1c20e44887f04037f7f68443199bf475
Andrew Pitt <andrew.pitt@canada.ca>
2017-07-22 14:42:04 -0400

    Adding left-nav page.

---
+++ src/main/java/ca/pitt/wet/cdts/demo/wetcdtsspringboot/controllers/LeftNavController.java
@@ -0,0 +1,26 @@
+package ca.pitt.wet.cdts.demo.wetcdtsspringboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Home Controller.
+ * @author Andrew Pitt
+ * @since 1.0.0
+ */
+@Controller
+@RequestMapping(path="/nav")
+public class LeftNavController {
+
+	/**
+	 * Get home.
+	 * @return <code>ModelAndView</code>
+	 */
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView getHome() {
+		return new ModelAndView("nav");
+	}
+	
+}
