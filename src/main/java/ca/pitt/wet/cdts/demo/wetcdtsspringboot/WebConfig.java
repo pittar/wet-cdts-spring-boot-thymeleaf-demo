6d396907fc4d08e14cebbe360214a4e5b04ecb1a
Andrew Pitt <andrew.pitt@canada.ca>
2017-07-23 15:31:11 -0400

    Fixed messages source and side menu. Added urls properties.

--- src/main/java/ca/pitt/wet/cdts/demo/wetcdtsspringboot/WebConfig.java
+++ src/main/java/ca/pitt/wet/cdts/demo/wetcdtsspringboot/WebConfig.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+import ca.canada.ised.wet.cdts.components.wet.interceptor.WETLocaleChangeInterceptor;
 import ca.canada.ised.wet.cdts.components.wet.interceptor.WETTemplateInterceptor;
 
 /**
@@ -19,28 +20,39 @@ import ca.canada.ised.wet.cdts.components.wet.interceptor.WETTemplateInterceptor
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-    /** The cdn template interceptor. */
-    @Autowired
-    private WETTemplateInterceptor cdnTemplateInterceptor;
-    
-    @Bean
-    public LocaleResolver localeResolver() {
-        CookieLocaleResolver slr = new CookieLocaleResolver();
-        slr.setDefaultLocale(Locale.CANADA);
-        return slr;
-    }
-    
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang");
-        return lci;
-    }
-    
-    /** {@inheritDoc} */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        super.addInterceptors(registry);
-        registry.addInterceptor(cdnTemplateInterceptor);
-    }
+	/** The WET CDTS template interceptor. */
+	@Autowired
+	private WETTemplateInterceptor cdnTemplateInterceptor;
+
+	/**
+	 * Locale resolver.
+	 * 
+	 * @return <code>LocaleResolver</code>
+	 */
+	@Bean
+	public LocaleResolver localeResolver() {
+		CookieLocaleResolver slr = new CookieLocaleResolver();
+		slr.setDefaultLocale(Locale.CANADA);
+		return slr;
+	}
+
+	/**
+	 * LocaleChangeInterceptor.
+	 * 
+	 * @return <code>LocaleChangeInterceptor</code>
+	 */
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		WETLocaleChangeInterceptor lci = new WETLocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		super.addInterceptors(registry);
+		registry.addInterceptor(localeChangeInterceptor());
+		registry.addInterceptor(cdnTemplateInterceptor);
+	}
 }
--- src/main/java/ca/pitt/wet/cdts/demo/wetcdtsspringboot/controllers/LeftNavController.java
+++ src/main/java/ca/pitt/wet/cdts/demo/wetcdtsspringboot/controllers/LeftNavController.java
@@ -7,20 +7,31 @@ import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Home Controller.
+ * 
  * @author Andrew Pitt
  * @since 1.0.0
  */
 @Controller
-@RequestMapping(path="/nav")
+@RequestMapping(path = "/nav")
 public class LeftNavController {
 
 	/**
-	 * Get home.
+	 * Get nav.
+	 * 
 	 * @return <code>ModelAndView</code>
 	 */
-	@RequestMapping(method=RequestMethod.GET)
-	public ModelAndView getHome() {
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView getNav() {
 		return new ModelAndView("nav");
 	}
-	
+
+	/**
+	 * Post nav.
+	 * 
+	 * @return <code>ModelAndView</code>
+	 */
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView postNav() {
+		return new ModelAndView("nav");
+	}
 }
