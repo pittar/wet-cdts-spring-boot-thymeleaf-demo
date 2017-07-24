7fcd922d0075c859a77d81491673bcce0d7889a4
Andrew Pitt <andrew.pitt@canada.ca>
2017-07-23 21:10:13 -0400

    Updated to use automated message source creation.

--- src/main/java/ca/pitt/wet/cdts/demo/wetcdtsspringboot/ApplicationConfig.java
+++ src/main/java/ca/pitt/wet/cdts/demo/wetcdtsspringboot/ApplicationConfig.java
@@ -1,13 +1,9 @@
 package ca.pitt.wet.cdts.demo.wetcdtsspringboot;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 
 /**
  * Application Config.
@@ -17,21 +13,6 @@ import org.springframework.context.support.ResourceBundleMessageSource;
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, H2ConsoleAutoConfiguration.class })
 public class ApplicationConfig {
-
-	/**
-	 * Application message source. Uses WET CDTS message source as its parent.
-	 * 
-	 * @param wetCDTSMessageSource
-	 *            <code>MessageSource</code>
-	 * @return <code>MessageSource</code>
-	 */
-	@Bean(name = "messageSource")
-	public MessageSource messageSource(@Qualifier("wetCDTSMessgeSource") MessageSource wetCDTSMessageSource) {
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setParentMessageSource(wetCDTSMessageSource);
-		messageSource.setBasenames("messages", "urls");
-		return messageSource;
-	}
 
 	/**
 	 * Main method.
