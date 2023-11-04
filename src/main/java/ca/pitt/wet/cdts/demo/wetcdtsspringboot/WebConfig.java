269f6fb167e76cd05fca6f82fadd8322989e5501
Jim Sellers <jim.sellers@gmail.com>
2023-11-04 15:50:16 -0400

    cleanup

--- src/main/java/ca/pitt/wet/cdts/demo/wetcdtsspringboot/WebConfig.java
+++ src/main/java/ca/pitt/wet/cdts/demo/wetcdtsspringboot/WebConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
